9f0bde616542c647cda78db14412853b176e23cc
Rogério Andrade <rgrandrade@bestdev.top>
2023-01-23 10:32:54 -0300

    .gitignore

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-095413544ce52437d"
+  instance_type = "t2.micro"
+  key_name = "oregon_"
+
+  tags = {
+    Name = "Primeira_Instancia"
+  }
+}
